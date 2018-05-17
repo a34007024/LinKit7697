b086a1d6f36d12ff768b69fa19e89a47775411b7
a34007024 <a34007024@gmail.com>
2018-05-17 19:02:19 +0800

    upload some practices
    增加了blink without delay function
    和fade呼吸燈~~~!

---
+++ 180517_BlinkWithoutDelay/180517_BlinkWithoutDelay.ino
@@ -0,0 +1,39 @@
+/*
+ * 以下是針對各型態的變數在Arduino裡的位元數說明
+int:
+https://www.arduino.cc/reference/en/language/variables/data-types/int/
+long:
+https://www.arduino.cc/reference/en/language/variables/data-types/long/
+unsigned int:
+https://www.arduino.cc/reference/en/language/variables/data-types/unsignedint/
+unsigned long:
+https://www.arduino.cc/reference/en/language/variables/data-types/unsignedlong/
+*/
+const int ledPin=2;//外接LED Pin腳
+const long timerDelay = 1000;//每1000ms改變LED狀態
+int ledState = LOW;//儲存LED目前狀態
+unsigned long previousTime = 0;//用來保存前一次LED狀態更新的時刻點
+//使用long是為了儲存比int長的整數
+//對Arduino來講long可儲存(-2^31-1 ~ 2^31-1) int為(-2^15-1 ~ 2^15-1)
+
+void setup() {
+  pinMode(ledPin,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);//內建LED
+  digitalWrite(ledPin,LOW);
+  digitalWrite(LED_BUILTIN,LOW);
+}
+void loop() {
+  unsigned long currentTime = millis();
+  digitalWrite(ledPin,ledState);
+  digitalWrite(LED_BUILTIN,ledState);
+  if(currentTime - previousTime >= timerDelay){
+    if(ledState == LOW){
+      ledState = HIGH;
+    }//轉換LED狀態
+    else ledState = LOW;
+    //轉換LED狀態
+    previousTime = currentTime;
+  }
+}
+
+
---
+++ 180517_fade/180517_fade.ino
@@ -0,0 +1,30 @@
+const int ledPin = 2;
+//use to connect with led+ pin
+int brightness = 0;
+//led brightness, from 0 to 255(min~max)
+int addValue=1;
+//the value to increase or decrease brightness
+//==============above are global variable===============
+void setup() {
+  //the code in setup() only run once when started
+  pinMode(ledPin,OUTPUT);
+  //set pin2 as OUTPUT pin
+  digitalWrite(ledPin,LOW);
+  //set output voltage to 0 at first
+  pinMode(LED_BUILTIN,OUTPUT);//built in led on board
+  digitalWrite(LED_BUILTIN,LOW);
+}
+void loop() {
+  //the code in loop() run repeatedly
+  analogWrite(ledPin,brightness);
+  //write value between 0~255 to pin2
+  analogWrite(LED_BUILTIN,brightness);
+  brightness += addValue;
+  //add "addValue" to brightness
+  if(brightness >= 70 || brightness <= 0){
+    //control brightness between 0-50
+    addValue = -addValue;
+    //reverse the "addValue" value
+  }
+  delay(30);
+}
