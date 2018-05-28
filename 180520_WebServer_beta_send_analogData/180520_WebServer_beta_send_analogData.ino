7623371267973d77e7b764e4a5da56ed68daa908
Luckypig <a34007024@gmail.com>
2018-05-28 21:08:38 +0800

    rename webserver_beta
    重新命名檔案，並新增了一些註解
    還有整理副程式及HTML語法

---
+++ 180520_WebServer_beta_send_analogData/180520_WebServer_beta_send_analogData.ino
@@ -0,0 +1,73 @@
+//參考Linkit7697的範例WiFiWebServer
+//WiFiWebServer把Linkit做為一個小型的伺服器使用
+#include <LWiFi.h>
+const char* ssid = "Linkit7697";//network SSID
+const char* password = "homecaresystem";//network password
+int keyIndex = 0;//network key Index number (needed only for WEP)
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+WiFiClient client = server.available();
+const int ledPin = 2;
+int ledState = LOW;
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ;//等待與電腦USB port通訊
+  }
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,ledState);
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  while (status != WL_CONNECTED) {
+    Serial.print(".");
+    status = WiFi.begin(ssid, password);
+    //Try to connect to WPA/WPA2 network.
+  }
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+void loop() {
+  client = server.available();// listen for incoming clients
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();//讀取客戶端的資訊
+        String request = String(c);
+        Serial.write(c);//把讀進來的客戶端資訊寫出來
+        // if you've gotten to the end of the line
+        //(received a newlinecharacter)
+        //and the line is blank, the http request has ended,
+        // so you can send a reply(send HTML to client)
+        if (c == '\n' && currentLineIsBlank) {
+          Serial.write("End of the data that client sended\n");
+          Serial.write("===============================\n");
+          sendHTMLheader();
+          sendHTML();
+          break;
+        }
+        if(request.indexOf("/ledChange") != -1){
+          if(ledState == LOW) ledState = HIGH;
+          else ledState = LOW;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
---
+++ 180520_WebServer_beta_send_analogData/HTMLforClient.ino
@@ -0,0 +1,25 @@
+void sendHTMLheader(){
+  // send a standard http response header
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");  // the connection will be closed after completion of the response
+  client.println("Refresh: 1");  // refresh the page automatically every 1 sec
+  client.println();
+  client.println("<!DOCTYPE HTML>");
+}
+
+void sendHTML(){
+  client.println("<html>");
+  // output the value of each analog input pin
+  for(int analogChannel = A0; analogChannel <= A3; analogChannel++) {
+    int sensorReading = analogRead(analogChannel);
+    client.print("analog input ");
+    client.print(analogChannel);
+    client.print(" is ");
+    client.print(sensorReading);
+    client.println("<br />");     
+  }
+  client.println("<a href=\"/ledChange\"\"><input type=\"button\" value=\"我是按鈕\" style=\"width:120px;height:40px;font-size:20px;\">");
+  //send a button to Control LED
+  client.println("</html>");//HTML結尾
+}
